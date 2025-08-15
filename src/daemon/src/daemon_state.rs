7824ac870bf793c80e52c0078e92873c48c60f86
arm1nt <armin.nikbakht@gmail.com>
2025-08-15 17:25:26 +0200

    Allow resetoring daemon state from the workspaces config file

--- src/daemon/src/daemon_state.rs
+++ src/daemon/src/daemon_state.rs
@@ -1,4 +1,6 @@
 use std::sync::{Arc, Mutex};
+use log::{info, warn};
+use crate::domain::models::WorkspaceInformation;
 use crate::monitor_manager::MonitorManager;
 use crate::util::error_exit;
 use crate::workspace_config::WorkspaceConfiguration;
@@ -27,7 +29,24 @@ impl DaemonState {
     }
 
     /// Restore the daemon's state by starting all workspaces specified in the ws config file
-    pub fn restore(&self) {
-        //todo!()
+    pub fn restore(&mut self) {
+        info!("Restoring daemon state from workspaces config file...");
+
+        let mut successful_starts: usize = 0;
+        let configured_workspaces: Vec<WorkspaceInformation> = self.ws_config.all();
+
+        for workspace in configured_workspaces.iter() {
+            match self.monitor_manager.start_monitor(workspace) {
+                Ok(()) => {
+                    successful_starts += 1;
+                    info!("Successfully started monitor for '{}'", workspace.name);
+                },
+                Err(e) => {
+                    warn!("Failed to start monitor for '{}': {e}", workspace.name);
+                }
+            }
+        }
+
+        info!("{}/{} monitors were started successfully!", successful_starts, configured_workspaces.len());
     }
 }
