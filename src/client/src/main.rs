56bdb6b8d6aaed9a2d1bd3b4f30177c26c85df00
arm1nt <armin.nikbakht@gmail.com>
2025-08-18 15:15:39 +0200

    Add banner

--- src/client/src/main.rs
+++ src/client/src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{Read, Write};
+use std::process;
+use clap::Parser;
+use crate::cli::Cli;
+use crate::mappers::ClientRequest;
+
+mod cli;
+mod mappers;
+
+fn print_banner() {
+    println!(r"
+          __            __
+    |  | /__` \ / |\ | /  `
+    |/\| .__/  |  | \| \__,
+   ");
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_banner();
+
+    let cli: Cli = Cli::parse();
+    let _request = ClientRequest::get_client_request(cli).unwrap_or_else(|e| {
+        eprintln!("[ERROR] {e}");
+        process::exit(1)
+    });
+
 }
